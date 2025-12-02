04c17a81d42f87bb3d8bd755d9d12325b6d783f0
AL <alcoran026@gmail.com>
2025-12-02 23:14:01 +0800

    [forms] Added functions for API integrations

---
+++ src/lib/services.ts
@@ -0,0 +1,39 @@
+import { Task } from "./model";
+
+export async function getTasks() {
+   const res = await fetch("/api/tasks");
+ 
+   if (!res.ok) {
+     throw new Error("Failed to fetch tasks");
+   }
+ 
+   return res.json();
+}
+
+export async function createTask(taskData: Task) {
+   const res = await fetch("/api/tasks", {
+      method: "POST",
+      headers: {
+         "Content-Type": "application/json",
+      },
+      body: JSON.stringify(taskData),
+   });
+ 
+   if (!res.ok) {
+     throw new Error("Failed to create task");
+   }
+ 
+   return res.json();
+}
+
+export async function deleteTask(taskId: number) {
+   const res = await fetch(`/api/tasks/${taskId}`, {
+     method: "DELETE",
+   });
+ 
+   if (!res.ok) {
+     throw new Error("Failed to delete task");
+   }
+ 
+   return res.json();
+}
