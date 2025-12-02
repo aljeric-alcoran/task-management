6fc3f8868a1eacd766367f34e95e4e62cd776ef1
AL <alcoran026@gmail.com>
2025-12-02 23:56:06 +0800

    [update-task] Added function to complete and incomplete a task

--- src/app/api/tasks/[taskId]/route.ts
+++ src/app/api/tasks/[taskId]/route.ts
@@ -5,7 +5,63 @@ const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export async function DELETE(req: Request, { params }: { params: Promise<{ taskId: string }> }) {
+export async function PUT(
+   req: NextRequest, 
+   { params }: { params: Promise<{ taskId: string }> }
+) {
+   try {
+      const { taskId } = await params;
+      const body = await req.json();
+   
+      const { data, error } = await supabase
+         .from("tasks")
+         .upsert({ id: taskId, ...body }, { onConflict: "id" })
+         .select()
+         .single();
+   
+      if (error) {
+         console.error("Supabase error:", error);
+         return NextResponse.json({ error: error.message }, { status: 500 });
+      }
+   
+      return NextResponse.json({ task: data, message: "Task updated successfully" }, { status: 200 });
+   } catch (error: any) {
+      console.error("Server error:", error);
+      return NextResponse.json({ error: "Server error" }, { status: 500 });
+   }
+}
+
+export async function PATCH(
+   req: NextRequest, 
+   { params }: { params: Promise<{ taskId: string }> }
+) {
+   try {
+      const { taskId } = await params;
+      const body = await req.json();
+   
+      const { data, error } = await supabase
+         .from("tasks")
+         .update(body)
+         .eq("id", taskId)
+         .select()
+         .single();
+   
+      if (error) {
+         console.error("Supabase error:", error);
+         return NextResponse.json({ error: error.message }, { status: 500 });
+      }
+   
+      return NextResponse.json({ task: data, message: "Task updated successfully" }, { status: 200 });
+   } catch (error: any) {
+      console.error("Server error:", error);
+      return NextResponse.json({ error: "Server error" }, { status: 500 });
+   }
+}
+
+export async function DELETE(
+   req: Request, 
+   { params }: { params: Promise<{ taskId: string }> }
+) {
    try {
       const { taskId } = await params;
       if (!taskId) {
--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,15 +1,18 @@
 "use client"
 
+import DeleteDialog from "./DeleteDialog";
+import FormDialog from "./FormDialog";
 import { ArrowDown, ArrowRight, ArrowUpRight, Pencil, Trash2 } from "lucide-react";
 import { Button } from "./ui/button";
 import { Checkbox } from "./ui/checkbox";
 import { Item, ItemActions, ItemContent, ItemDescription, ItemMedia, ItemTitle } from "./ui/item";
 import { Badge } from "./ui/badge";
-import DeleteDialog from "./DeleteDialog";
 import { useState } from "react";
-import FormDialog from "./FormDialog";
 import { Task } from "@/lib/model";
 import { formatDateWithOrdinal } from "@/lib/helpers";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { completeTask } from "@/lib/services";
+import { toast } from "sonner";
 
 export default function TaskCard({ task }: { task: Task; }) {
    const priorityClasses = {
@@ -28,11 +31,40 @@ export default function TaskCard({ task }: { task: Task; }) {
    
    const [open, setOpen] = useState<boolean>(false);
    const [update, setUpdate] = useState<boolean>(false);
+   const [isCompleted, setIsCompleted] = useState<boolean>(task.completed);
+
+   const queryClient = useQueryClient();
+   
+   type Payload  = { taskId: number; completed: boolean };
+
+   const completeTaskAction = useMutation({
+      mutationFn: ({ taskId, completed }: Payload) => completeTask(taskId, completed),
+      onSuccess: (data) => {
+         setIsCompleted(data.task.completed);
+         queryClient.invalidateQueries({ queryKey: ["tasks"] });
+         toast.success("Success!", { description: `Task marked as ${!isCompleted ? "completed"  : "incomplete"}.` });
+      },
+      onError: (error: any) => {
+         toast.error("Error!", { description: error.message });
+      },
+   });
+
+   async function onCompleteTask(event: boolean) {
+      const result = await completeTaskAction.mutateAsync({ taskId: task.id!, completed: event });
+      console.log(result);
+   }
+
    return(
       <Item variant="outline" size="sm" asChild>
          <div>
             <ItemMedia>
-               <Checkbox className="data-[state=checked]:bg-teal-600" id={`terms-${task.id}`} checked={task.completed}/>
+               <Checkbox 
+                  disabled={completeTaskAction.isPending}
+                  onCheckedChange={onCompleteTask}
+                  className="data-[state=checked]:bg-teal-600" 
+                  id={`terms-${task.id}`} 
+                  checked={isCompleted}
+               />
             </ItemMedia>
             <ItemContent>
                <ItemTitle className={`${task.completed ? "line-through" : ""}`}>{task.task_title}</ItemTitle>
