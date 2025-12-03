0e156761c644b0cf45c2e720f8274e5fde4751d3
AL <alcoran026@gmail.com>
2025-12-04 00:11:39 +0800

    [update-task] Refactor schema to fix unknown issue

--- src/schema/task.schema.ts
+++ src/schema/task.schema.ts
@@ -6,8 +6,8 @@ import { Task } from "@/lib/model";
 export const taskFormSchema = z.object({
    task_title: z.string().trim().min(1, "Task title is required."),
    description: z.string().min(1, "Description is required."),
-   priority: z.string("Please select task priority."),
-   due_date: z.date("Select the task's completion date."),
+   priority: z.enum(["high", "medium", "low"]).describe("Please select task priority."),
+   due_date: z.date(),
    completed: z.boolean(),
 });
 
@@ -17,10 +17,11 @@ export function useTaskFormSchema(task?: Task): UseFormReturn<TaskFormSchema> {
    return useForm<TaskFormSchema>({
       resolver: zodResolver(taskFormSchema),
       defaultValues: {
-         task_title: "",
-         description: "",
-         completed: false,
-         priority: task?.priority ?? ""
+         task_title: task?.task_title ?? "",
+         description: task?.description ?? "",
+         priority: task?.priority ?? "medium",
+         due_date: task?.due_date ? new Date(task.due_date.replace(" ", "T")) : new Date(),
+         completed: task?.completed ?? false,
       },
    });
 }
