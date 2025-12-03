aa45db4c16113d1eb5dbc08bb43d46e050872c35
AL <alcoran026@gmail.com>
2025-12-04 00:09:55 +0800

    [update-task] Refactor helper and added a new helper for matching objects

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,16 +1,25 @@
 export function formatDateWithOrdinal(date: string) {
-   const day = new Date(date).getDate();
-   const month = new Date(date).toLocaleString("en-US", { month: "long" });
-   const year = new Date(date).getFullYear();
- 
-   const ordinal =
-      day % 10 === 1 && day !== 11
-         ? "st"
-         : day % 10 === 2 && day !== 12
-         ? "nd"
-         : day % 10 === 3 && day !== 13
-         ? "rd"
-         : "th";
- 
-   return `${month} ${day}${ordinal}, ${year}`;
+   const d = new Date(date);
+
+   const month = d.toLocaleString("en-US", { month: "long" });
+   const day = d.getDate().toString().padStart(2, "0");
+   const year = d.getFullYear();
+
+   let hours = d.getHours();
+   const minutes = d.getMinutes().toString().padStart(2, "0");
+   const ampm = hours >= 12 ? "PM" : "AM";
+
+   hours = hours % 12;
+   hours = hours === 0 ? 12 : hours; // convert 0 to 12
+   const hoursStr = hours.toString().padStart(2, "0");
+
+   return `${month} ${day}, ${year} ${hoursStr}:${minutes}${ampm}`;
+}
+
+export const isObjectSharedKeyMatched = <
+   T extends Record<string, any>,
+   U extends Record<string, any>
+>(obj1: T, obj2: U): boolean => {
+   const sharedKeys = Object.keys(obj1).filter((key) => key in obj2);
+   return sharedKeys.every(key => obj1[key] === obj2[key]);
 }
