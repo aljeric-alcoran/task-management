aa44fbd577693831a1f6eecb7f075c5aac556c2a
AL <alcoran026@gmail.com>
2025-12-04 11:24:45 +0800

    [prisma-v-7] Refactor prisma client by using the latest version

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,24 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaMariaDb } from '@prisma/adapter-mariadb'
 
-const prisma = new PrismaClient();
+const adapter = new PrismaMariaDb({
+   host: process.env.NEXT_PUBLIC_MYSQL_HOST,
+   port: Number(process.env.NEXT_PUBLIC_MYSQL_PORT),
+   user: process.env.NEXT_PUBLIC_MYSQL_USER,  
+   password: process.env.NEXT_PUBLIC_MYSQL_PASSWORD,
+   database: process.env.NEXT_PUBLIC_MYSQL_DATABASE,
+   connectionLimit: 5
+})
 
-export { 
-   prisma 
-};
+const prismaClientSingleton = () => {
+   return new PrismaClient({ adapter })
+}
+
+declare const globalThis: {
+   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export default prisma
+if (process.env.NEXT_PUBLIC_ENVIRONMENT !== 'production') globalThis.prismaGlobal = prisma
